9ccada0bc84d16f53e18d11d50159c6afd24fc67
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-07-04 15:34:39 +0530

    max methos to take more then three parameters

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
-public class TestMaximum {
+public class TestMaximum<T> {
     public static void main(String[] args) {
         TestMaximum testMaximum = new TestMaximum();
-        testMaximum.getMaximum(23.5F,235.5F, 234.333F);
+        Scanner in = new Scanner(System.in);
+
+        
 
     }
     public Integer getMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
@@ -33,11 +35,21 @@ public class TestMaximum {
         }
         else if(s2.length() <= s1.length() && s2.length() <= s3.length()){
             System.out.println(s2);
-            return s2
+            return s2;
         }
         else {
             System.out.println(s3);
             return s3;
         }
+    }
+    public <T extends Comparable<T>> void Maximum(T val1, T val2, T val3){
+        T max = val1;
+        if(val3.compareTo(max)<0){
+            max = val3;
+        }
+        if(val2.compareTo(max) < 0){
+            max = val2;
+        }
+        System.out.println(max);
     }
 }
